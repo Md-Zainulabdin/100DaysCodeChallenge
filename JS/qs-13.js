fb68150f28e1a76335b74130316ac72a3ccca243
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-12-21 22:34:25 +0500

    solved question 13

---
+++ JS/qs-13.js
@@ -0,0 +1,10 @@
+// 13 ->  Question: Check if a number is even or odd and return a corresponding message.
+
+const input = 7;
+
+if (input % 2 === 0) {
+    console.log("Even");
+}
+else {
+    console.log("Odd");
+}
