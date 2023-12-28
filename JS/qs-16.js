76e03fb87ee93ac0950c590caced95956f5f7115
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-12-28 21:33:00 +0500

    solved question 16

---
+++ JS/qs-16.js
@@ -0,0 +1,16 @@
+// Q -> 16
+
+// 1. Rewrite the following code using a ternary operator:
+// let result;
+// if (score >= 80) {
+//     result = "Pass";
+// } else {
+//     result = "Fail";
+// }
+
+// See here is my code,
+
+let score = 100;
+
+let result = score >= 80 ? "Pass" : "Fail";
+console.log("result", result);
