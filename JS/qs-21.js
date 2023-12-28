baf0dd502c5ad9b196e747a0b81c4457e7e9de58
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-12-28 22:13:45 +0500

    solved question 21

---
+++ JS/qs-21.js
@@ -0,0 +1,10 @@
+// 21 -> Create an object car with properties make, model, and a method startEngine that logs a message.
+// Instantiate the object and call the method.
+
+const car = {
+    make: "Lamborghini",
+    model: "Huracán",
+    startEngine: () => console.log("Engine started, Bhoom Bhoom"),
+}
+
+car.startEngine();
