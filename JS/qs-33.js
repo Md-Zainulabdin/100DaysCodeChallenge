0c84dc06bf5dcf39c299856671c1f852f353af02
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2024-01-01 21:52:39 +0500

    solved qs-33

---
+++ JS/qs-33.js
@@ -0,0 +1,20 @@
+/*Sorting Objects:*
+   32 -> Q: Given an array of objects with a 'price' property, use the sort method to arrange 
+   them in descending order based on their prices.*/
+
+const products = [
+    {
+        name: "Iphone", price: 5000, currency: "USD",
+    }, {
+        name: "Macbook", price: 11000, currency: "USD",
+    }, {
+        name: "Airpods", price: 1200, currency: "USD",
+    }, {
+        name: "Ipad", price: 3000, currency: "USD",
+    }, {
+        name: "IWatch", price: 800, currency: "USD",
+    }
+]
+
+const sortedProducts = products.sort((a, b) => b.price - a.price);
+console.log(sortedProducts);
